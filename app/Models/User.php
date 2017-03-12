05c3dbbeb48f768e1a29358be7c9a53079645a9e
Faozi Unnes <faozimipa@gmail.com>
2017-03-12 06:48:14 +0000

    add User API

---
+++ app/Models/User.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class User
+ * @package App\Models
+ * @version March 12, 2017, 6:26 am UTC
+ */
+class User extends Model
+{
+    use SoftDeletes;
+
+    public $table = 'users';
+    
+
+    protected $dates = ['deleted_at'];
+
+
+    public $fillable = [
+        'name',
+        'email',
+        'password'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'name' => 'string',
+        'email' => 'string',
+        'password' => 'string'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'name' => 'required',
+        'email' => 'required|email',
+        'password' => 'required'
+    ];
+
+    
+}
